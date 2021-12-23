b59cb938729398d15ea90894136a979847b02669
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-12-23 20:23:02 +0000

    functional tabs

--- tabs_controller.js
+++ tabs_controller.js
@@ -1,7 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "tab", "tabContent" ]
+
   connect() {
-    console.log("Hello from tabs controller!")
+    this.displayContent()
+  }
+
+  switch() {
+    this.toggleActiveClass()
+    this.displayContent()
+  }
+
+  toggleActiveClass() {
+    this.tabTargets.forEach(tab => {
+      tab.classList.toggle('active', tab === event.currentTarget)
+    })
+  }
+
+  displayContent() {
+    this.tabTargets.forEach((tab, index) => {
+      const content = this.tabContentTargets[index]
+      if (!tab.classList.contains('active')) {
+        content.hidden = true
+      } else {
+        content.hidden = false
+      }
+    })
   }
 }
